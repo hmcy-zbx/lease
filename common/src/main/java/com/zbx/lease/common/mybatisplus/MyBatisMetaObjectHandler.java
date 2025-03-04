49fede36a3226d6adff75fcb8bc4ef63a70d60d8
zbx <1060810584@qq.com>
2025-03-04 11:55:20 +0800

    公寓支付方式功能实现

---
+++ common/src/main/java/com/zbx/lease/common/mybatisplus/MyBatisMetaObjectHandler.java
@@ -0,0 +1,20 @@
+package com.zbx.lease.common.mybatisplus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyBatisMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject,"createTime", Date.class,new Date());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject,"updateTime", Date.class, new Date());
+    }
+}
